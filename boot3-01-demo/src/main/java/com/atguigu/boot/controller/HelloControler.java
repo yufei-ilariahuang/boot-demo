84c37a11a2ffbe3bde334640b01f7fc5b1731532
lia Ha <fayyyufei@gmail.com>
2024-07-01 17:36:21 -0700

    helo controler

---
+++ boot3-01-demo/src/main/java/com/atguigu/boot/controller/HelloControler.java
@@ -0,0 +1,24 @@
+package com.atguigu.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.GetExchange;
+
+/**
+ * @author faye
+ * @Description
+ * @create 2024-07-01
+ */
+//@Controller
+//@ResponseBody
+@RestController
+public class HelloControler {
+
+    @GetMapping("hello")
+    public String hello(){
+        return "Halo, Spring Boot 3!";
+    }
+
+}
